b9d2f05e8f34a8dbf42663dca8ed5763a9393ba0
Lasse Harm <lasseharm@web.de>
2019-02-10 17:22:53 +0100

    Anleitung zum kompiliren erstellt0

---
+++ 01/dataTypes.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+/*
+    In der Programmiersprache C++ gibt
+    es verschiedene Datentypen welche 
+    verschiedebe Eigenschaften abbilden:
+*/
+
+void f_Integer()
+{
+    
+}
+
+int main(void)
+{
+    return 0;
+}
